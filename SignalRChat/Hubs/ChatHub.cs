23b3c992f436496a5872043232a13a5b7a00d569
Hamid <hamidprime@gmail.com>
2022-04-23 21:13:43 +0430

    Add project files.

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+        public void Login(string userId)
+        {
+            if(!users.ContainsKey(userId))
+                users.Add(userId, Context.ConnectionId);
+            else
+                users[userId] = Context.ConnectionId;
+        }
+        public async Task SendMessage(string user, string toUser, string message)
+        {
+            if(users.ContainsKey(toUser))
+                await Clients.Client(users[toUser]).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
